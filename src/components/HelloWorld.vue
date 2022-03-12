a00801f9bd74d786f1cee4690e64cd4700e9717b
AndhikaRahmatullah <dikarahmatullah909@gmail.com>
2022-03-13 05:19:29 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,42 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="text-center">
+		<p>
+			Nama <span>{{ nama }}</span>
+		</p>
+		<p>
+			Umur <span>{{ umur }}</span>
+		</p>
+		<p>
+			Semester <span>{{ semester }}</span>
+		</p>
+		<!-- Mengirim data dari child(HelloWord) ke parent(HomeView) -->
+		<button
+			@click="changeSemester"
+			class="bg-cyan-500 p-2 rounded-lg mt-3"
+		>
+			Ubah Semester
+		</button>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	export default {
+		name: `HelloWorld`,
+		// props berisi array
+		// Mengirim data dari parent(HomeView) ke child(HeloWord)
+		props: [`nama`, `umur`, `semester`],
+		data() {
+			return {
+				newSemester: `Lulus !`,
+			};
+		},
+		methods: {
+			// methods berisi function
+			// Mengirim data dari child(HelloWord) ke parent(HomeView)
+			changeSemester() {
+				// jika event di click akan menghasilkan data newSemester
+				this.$emit("changeSemester", this.newSemester);
+			},
+		},
+	};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="text-center">
+		<img alt="Vue logo" src="../assets/logo.png" class="mx-auto" />
+	</div>
+	<div class="">
+		<!-- mengirim data nama dan umur dari parent(HomeView) ke child(HelloWord) -->
+		<!-- bind data semester -->
+		<HelloWorld
+			nama="Andhika"
+			umur="20"
+			@changeSemester="changeSemester"
+			:semester="semester"
+		/>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	// import file child
+	import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "HomeView",
+		data() {
+			return {
+				// bind data semester
+				semester: `6 (enam)`,
+			};
+		},
+		components: {
+			// panggil nama component
+			HelloWorld,
+		},
+		methods: {
+			// setiap ada event pasti ada methods;
+			// methods berisi function dan beri parameter untuk aski
+			changeSemester(newSemester) {
+				// jika di klik ubah this.semester menjadi newSemester
+				this.semester = newSemester;
+			},
+		},
+	};
 </script>
